7417cadb63e148d10bd21167f5c64f0dd1e07301
Juan Cataldo <jcataldoaguilera@gmail.com>
2024-03-19 17:47:48 -0300

    primera version mayor_a.py

---
+++ Act1/mayor_a.py
@@ -0,0 +1,35 @@
+# Info
+# Autor: Juan Cataldo [jcataldoaguilera@gmail.com]
+# Fecha: 2024-03-19
+# RLAB-23-02-09-0044-2
+#
+
+# Balances del año anterior
+ventas = {
+"Enero": 15000,
+"Febrero": 22000,
+"Marzo": 12000,
+"Abril": 17000,
+"Mayo": 81000,
+"Junio": 13000,
+"Julio": 21000,
+"Agosto": 41200,
+"Septiembre": 25000,
+"Octubre": 21500,
+"Noviembre": 91000,
+"Diciembre": 21000,
+}
+
+# Librerias
+from sys import argv
+
+#
+i = int(argv[1])
+filtro = {}
+
+for k,v in ventas.items() :
+    if v > i :
+        salida = {k:v}
+        filtro.update(salida)
+
+print(filtro)
