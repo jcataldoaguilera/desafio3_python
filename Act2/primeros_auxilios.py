da15104934f88ef99b068ca99af7a3e701068eab
Juan Cataldo <jcataldoaguilera@gmail.com>
2024-03-19 22:30:34 -0300

    se entrega primeros_auxilios.py

--- Act2/primeros_auxilios.py
+++ Act2/primeros_auxilios.py
@@ -4,3 +4,19 @@
 # RLAB-23-02-09-0044-2
 #
 
+# Programa
+if input("¿Responde a estímulos? (S/N): ").lower() == "s" :
+    print("+ Valorar la necesidad de llevarlo al hospital más cercano +")
+else :
+    print("Abrir la vía Aerea")
+    if input("¿Respira?: (S/N) ").lower() == "s" :
+        print("+ Permitirle posición de suficiente ventilación +")
+    else :
+        print("Administrar 5 Ventilaciones y llamar a la Ambulancia")
+        amb = "n"
+        while amb == "n" :
+            if input("¿Signos de Vida?: (S/N) ").lower() == "s" :
+                print("Reevaluar a la espera de la ambulancia")
+            else :
+                print("Administrar compresiones toraxicas hasta que llegue la ambulancia")
+            amb = input("¿Llegó la ambulancia?: (S/N)").lower()
---
+++ Act3/fuerza_bruta.py
@@ -0,0 +1,5 @@
+# Info
+# Autor: Juan Cataldo [jcataldoaguilera@gmail.com]
+# Fecha: 2024-03-19
+# RLAB-23-02-09-0044-2
+#
